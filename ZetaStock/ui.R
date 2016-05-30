b14e1020d0e009d91477ad5805d2b4a76095ad9c
0xFireball <0xFireball@outlook.com>
2016-05-30 12:16:41 -0700

    update some UI text

--- ZetaStock/ui.R
+++ ZetaStock/ui.R
@@ -18,21 +18,21 @@ shinyUI(
                sidebarLayout(
                  tags$img(src="https://raw.githubusercontent.com/ZetaPhase/ZetaKrypt/master/ZetaKrypt/img/ZetaKrypt-logo.png", width="256", height="256"),
                  mainPanel(
-                   h3(tags$p("Welcome to ZetaStock.")),
+                   h3(tags$p("Welcome to ZetaStock, a stock analyzer developed by ZetaPhase Technologies.")),
                    textInput(
                      inputId = "code",
                      label = NULL,
                      value = "",
                      width = "400px",
-                     placeholder = "Please enter the company code"
+                     placeholder = "Enter a company's stock code to get started"
                    ),
                    actionButton("go", "Go"),
                    h4(tags$p("About")),
                    htmlOutput("home"),
                    h4(tags$p("Links")),
-                   tags$a(href="https://github.com/ZetaPhase/ZetaStock", "Visit the GitHub Repo"),
+                   tags$a(href="https://github.com/ZetaPhase/ZetaStock", "Check out the source on GitHub"),
                    tags$br(),
-                   tags$a(href="https://zetaphase.io/", "Visit our main site page")
+                   tags$a(href="https://zetaphase.io/", "Visit the ZetaPhase Technologies website")
                  )
                )
              ),
